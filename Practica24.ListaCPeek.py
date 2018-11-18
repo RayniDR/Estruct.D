833f817dd274bcb02fe85a81b2be2ec668a92bc3
RayniDR <42855782+RayniDR@users.noreply.github.com>
2018-11-17 18:51:12 -0800

    Add files via upload

---
+++ Practica24.ListaCPeek.py
@@ -0,0 +1,74 @@
+class Nodo():# clase que sirve de molde para cada nodo
+	def __init__(self,dato):
+		self.next=None
+		self.dato=dato
+class ListaCircular():# clase para crear y enlazar los nodos
+	def __init__(self):# crea una raiz vacia
+		self.root=None
+
+	def push(self,dato):# funcion para agregar lo que recibe en sus parametros
+		if not self.root:# si la raiz de la lista no existe
+			self.root=Nodo(dato)# crea un nuevo nodo en la raiz
+			self.root.next=self.root# pone el puntero siguiente hacia si mismo, por ser circular la lista y ser el unico nodo existente
+		else:# si ya existe la raiz
+			new_nodo=Nodo(dato)# variable que almacena el nuevo nodo a agregar
+			actual=self.root# pone un puntero en la raiz
+			while actual.next!=self.root:# mientras el next del puntero no apunte a la raiz, o sea no sea el ultimo nodo
+				actual=actual.next# avanza en uno el punteor
+			actual.next=new_nodo# cuando acaba de recorrer la lista pone el next del ultimo nodo al nuevo nodo
+			new_nodo.next=self.root# pone el puntero del nuevo nodo a la raiz
+
+	def peek(self,selec):
+		cont=0
+		actual=self.root
+		if selec==1:#condicion para imprimir toda la lista
+			self.impresion=[]
+			actual=self.root
+			while actual:
+				self.impresion.append(actual.dato)
+				actual=actual.next
+				if actual==self.root:
+					break
+			print(self.impresion)
+		elif selec==2:# condicion para imprimir la raiz de la lista
+			print("La raiz es "+ str (self.root.dato))
+		elif selec==3:# condicion para imprimir el ultimo elemento de la lista
+			while actual.next:
+				temp=actual# guarda el nodo antes de avanzar de nuevo a la raiz para poder imprimirlo
+				actual=actual.next
+				if actual==self.root:
+					break
+			print("El ultimo elemento es "+str(temp.dato))
+		elif selec==4:# condicion para imprimir el indice de un elemento
+			cont=0# se crea un contador que es el que guarda el # de indice
+			num=int(input("Ingrese el elemento a buscar "))# se pide el numero a buscar
+			while actual:# mientras aun existan nodos
+			
+				if actual.dato==num:# si el dato del nodo actual es igual al dato que se busca, imprime el contador
+					print("El nodo del elemento ")+str(num)+(" es: ")+str(cont)
+				
+				
+					actual=None
+				elif actual.next==None:# si ya no hay nodos, imprime que no existe el elemento en la lista
+					print("El elemento ")+str(num)+(" no se encuentra en ningun nodo")
+					actual=None
+				else:# si aun hay nodos solo pasa al siguiente nodo y aumenta el contador
+					actual=actual.next
+					cont+=1
+lista=ListaCircular()
+while  True:
+	print("**************************\nMenu ListaCircular")
+	print("1-Agregar un nuevo nodo\n2-Metodo Peek\n3-Salir del programa")
+	selec=int(input("Por favor ingrese su seleccion: "))
+	if selec==1:
+		dato=int(input("Ingrese el elemento a agregar: "))
+		lista.push(dato)
+	if selec==2:
+		print("1-Imprimir toda la lista\n2-Imprimir la raiz de la lista\n3-Imprimir el ultimo elemento\n4-Buscar el indice de un elemento")
+		opc=int(input("Por favor ingrese su seleccion: "))
+		if opc==1 or opc==2 or opc==3 or opc==4:
+			lista.peek(opc)
+		else:
+			print("Opcion no valida")
+	if selec==3:
+		break
